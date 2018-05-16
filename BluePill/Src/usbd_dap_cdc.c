0f6ff2ca2c9f38faa17bcd7db1417dc6df337ebb
Pau Chafer <pau.chafer@bf1systems.com>
2018-05-16 17:19:18 +0100

    changed self powered with bus powered

--- BluePill/Src/usbd_dap_cdc.c
+++ BluePill/Src/usbd_dap_cdc.c
@@ -46,9 +46,10 @@ static uint8_t USBD_DAP_CDC_CfgDesc[USB_DAP_CDC_CONFIG_DESC_SIZ] =
   0x03,         /*bNumInterfaces: CDC:2 HID:1 interface*/
   0x01,         /*bConfigurationValue: Configuration value*/
   0x04,         /*iConfiguration: Index of string descriptor describing the configuration*/
-  0xC0,         /*bmAttributes: bus powered */
+//  0xC0,         /*bmAttributes: bus powered */
+  0x80,         /*bmAttributes: self powered no! */
   0x32,         /*MaxPower 100 mA: this current is used for detecting Vbus*/
-  /* 09 */
+  /* 09 */ /* 0x32 100 ma 0xFA 500 ma*/
 
 #if 1
   /************** Descriptor of DAP(HID) interface ****************/
